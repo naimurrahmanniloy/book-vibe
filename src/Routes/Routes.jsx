77d82203c42b2002411c0a3aad25d0fb19dae1a0
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2026-04-09 00:02:10 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Homepage from "../pages/Homepage/Homepage";
+import Books from "../pages/Books/Books";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/books",
+        element: <Books />,
+      },
+    ],
+  },
+]);
